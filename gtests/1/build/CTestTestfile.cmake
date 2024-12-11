1efe255fe3c9e43d5d0ca6c7746fe1290c7cff15
NikaSc <nika.shc1@gmail.com>
2024-12-11 19:12:05 +0300

    to_github2

--- gtests/1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matebook14/Документы/453501/ОАиП/LR6/gtests/1
-# Build directory: /home/matebook14/Документы/453501/ОАиП/LR6/gtests/1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[add]=] "a.out")
-set_tests_properties([=[add]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/matebook14/Документы/453501/ОАиП/LR6/gtests/1/CMakeLists.txt;11;add_test;/home/matebook14/Документы/453501/ОАиП/LR6/gtests/1/CMakeLists.txt;0;")
